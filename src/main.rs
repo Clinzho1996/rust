707fc3ceda8320b6aaf586cfe11ab6675989391b
clinzho1996 <confidinho@yahoo.com>
2026-09-25 15:34:10 +0100

    functions & expression

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 fn main () {
 human_id("Alice", 30, 165);
+add(5, 10);
+
+let y:i32 = add(5, 10);
+println!("Y is {}", y);
+
+let _X: i32 = {
+    let price: i32 = 100;
+    let tax: i32 = 10;
+    price + tax
+};
+
+println!("X is {}", _X);
+
+//calling bmi function
+let weight: f64 = 92.0; // in kilograms
+let height: f64 = 1.74; // in meters
+let bmi: f64 = calculate_bmi(weight, height);
+println!("My BMI is {:.2}", bmi);
 }
 
 
@@ -11,3 +29,13 @@ fn human_id(name: &str, age: u32, height: u32) -> String {
 
 //expressions and statements
 //expressions evaluate to a value, statements do not
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+
+//BMI
+fn calculate_bmi(weight: f64, height: f64) -> f64 {
+    weight / (height * height)
+}
