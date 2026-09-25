dedae2bf3fe4556ff1f3bf3c1619e6e01a58d6cc
clinzho1996 <confidinho@yahoo.com>
2026-09-25 16:02:23 +0100

    variables

--- src/main.rs
+++ src/main.rs
@@ -31,30 +31,41 @@
 
 //structs in Rust are a way to create custom data types that can hold multiple values. Structs can have fields of different types, and they can be used to model real-world entities.
 
-struct BankAccount {
-    owner: String,
-    account_number: String,
-    balance: f64,
-}
+// struct BankAccount {
+//     owner: String,
+//     account_number: String,
+//     balance: f64,
+// }
 
-fn withdraw(account: &mut BankAccount, amount: f64) {
-    if account.balance >= amount {
-        account.balance -= amount;
-        println!(" Account owned by {}", account.owner);
-        println!("Withdrawal successful. New balance: {}", account.balance);
-    } else {
-        println!(" Account owned by {}", account.owner);
-        println!("Insufficient funds. Current balance: {}", account.balance);
-    }
-}
+// fn withdraw(account: &mut BankAccount, amount: f64) {
+//     if account.balance >= amount {
+//         account.balance -= amount;
+//         println!(" Account owned by {}", account.owner);
+//         println!("Withdrawal successful. New balance: {}", account.balance);
+//     } else {
+//         println!(" Account owned by {}", account.owner);
+//         println!("Insufficient funds. Current balance: {}", account.balance);
+//     }
+// }
+
+// fn main() {
+//     let mut account = BankAccount {
+//         owner: "John Doe".to_string(),
+//         account_number: "123456789".to_string(),
+//         balance: 1000.0,
+//     };
+
+//     withdraw(&mut account, 1200.0);
+//     withdraw(&mut account, 200.0);
+// }
+
+//variables and mutability in Rust are important concepts that determine how data can be modified. By default, variables in Rust are immutable, meaning their values cannot be changed after they are assigned. However, you can make a variable mutable by using the mut keyword.
 
 fn main() {
-    let mut account = BankAccount {
-        owner: "John Doe".to_string(),
-        account_number: "123456789".to_string(),
-        balance: 1000.0,
-    };
-
-    withdraw(&mut account, 1200.0);
-    withdraw(&mut account, 200.0);
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
 }
+
+//constants in Rust are similar to variables, but they are always immutable and must have a type annotation. Constants are defined using the const keyword and can be declared in any scope, including the global scope. They are typically used for values that should not change throughout the program.
